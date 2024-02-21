90ee2bd920810a0ede0ab59d336a0a3bfaa5f03d
ricardolu1s <113566864+ricardolu1s@users.noreply.github.com>
2024-02-21 19:44:22 +0800

    chore: fix sql and ipy

--- operations/pivot/pivot.sql
+++ operations/pivot/pivot.sql
@@ -1,30 +1,32 @@
---base
+--count of patients by gender per age group
+SELECT 
+    age_group,
+    COUNT(CASE WHEN gender = 'MALE' THEN 1 END) AS male_patients,
+    COUNT(CASE WHEN gender = 'FEMALE' THEN 1 END) AS female_patients
+FROM (
+    SELECT 
+        CASE 
+            WHEN age BETWEEN 0 AND 14 THEN 'Children'
+            WHEN age BETWEEN 15 AND 24 THEN 'Youth'
+            WHEN age BETWEEN 25 AND 44 THEN 'Adults'
+            WHEN age BETWEEN 45 AND 64 THEN 'Middle-aged Adults'
+            WHEN age BETWEEN 65 AND 100 THEN 'Seniors'
+        END AS age_group,
+        gender
+    FROM 
+        px
+) AS age_gender_filtered
+GROUP BY 
+    age_group;
+
+--count of doctors per age group
 SELECT 
   CASE 
-    WHEN px.age BETWEEN 0 AND 14 THEN 'Children'
-    WHEN px.age BETWEEN 15 AND 24 THEN 'Youth'
-    WHEN px.age BETWEEN 25 AND 44 THEN 'Adults'
-    WHEN px.age BETWEEN 45 AND 64 THEN 'Middle-aged Adults'
-    WHEN px.age BETWEEN 65 AND 100 THEN 'Seniors'
-  END AS age_group,
-  SUM(CASE WHEN px.gender = 'MALE' THEN 1 ELSE 0 END) AS male_patients,
-  SUM(CASE WHEN px.gender = 'FEMALE' THEN 1 ELSE 0 END) AS female_patients
-FROM px
-WHERE px.age != 'None'
-GROUP BY age_group;
---improved null condition
-SELECT 
-  CASE 
-    WHEN px.age BETWEEN 0 AND 14 THEN 'Children'
-    WHEN px.age BETWEEN 15 AND 24 THEN 'Youth'
-    WHEN px.age BETWEEN 25 AND 44 THEN 'Adults'
-    WHEN px.age BETWEEN 45 AND 64 THEN 'Middle-aged Adults'
-    WHEN px.age BETWEEN 65 AND 100 THEN 'Seniors'
-  END AS age_group,
-  SUM(px.gender = 'MALE') AS male_patients,
-  SUM(px.gender = 'FEMALE') AS female_patients
-FROM px
-WHERE px.age IS NOT NULL  -- Improved null check
+    WHEN doctors.age BETWEEN 25 AND 44 THEN 'Adults'
+    WHEN doctors.age BETWEEN 45 AND 64 THEN 'Middle-aged Adults'
+    WHEN doctors.age BETWEEN 65 AND 100 THEN 'Seniors'
+  END AS age_group, COUNT(doctors.doctorid) AS num_doctors
+FROM doctors
 GROUP BY age_group;
 
 --count of hospitals and clinics per region
@@ -35,11 +37,7 @@ SELECT
     COUNT(CASE WHEN clinics.IsHospital = 0 THEN 1 END) AS num_clinics
 FROM clinics
 GROUP BY RegionName
+ORDER BY num_hospitals
 
---count of total hospitals and clinics
 
-SELECT 
-    
-    COUNT(CASE WHEN clinics.IsHospital = 1 THEN 1 END) AS num_hospitals,
-    COUNT(CASE WHEN clinics.IsHospital = 0 THEN 1 END) AS num_clinics
-FROM clinics
+
